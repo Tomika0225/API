2761c02372256a12a6ed44a97a8ae315cab91d20
Tomika0225 <72458257331@szily.hu>
2025-03-24 09:58:43 +0100

    dbcontroller elkészült

---
+++ dbcontroller.php
@@ -0,0 +1,44 @@
+<?php
+
+class DBController
+{
+    private $conn = null;
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $database = "mobile";
+
+}
+
+function __construck()
+{
+    $this->connectDB();
+}
+
+function connectDB(){
+
+
+    try {
+        $this->conn = new PDO("mysqli:host={$this->host}
+        dbname={$this->database}; charset=utf8",$this->user; $this->password);
+        $this->cpnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION)
+    } catch (PDOExeption $err) {
+        die("coonection failed " . $err->getMessage());
+    }
+
+    function executeSelectWuery($query, $params[])
+    {
+
+        try {
+            $stmt=$this->conn->prepare($query);
+            $stmt->exe($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } 
+        catch (PDOExeption $err)
+        {
+            die("Query Failed" . $err->getMessage());    
+        }
+    }
+}
+
+?>
